5ba4084254e2ed8902bd9882e1345c2e34978c58
NANDINI GAUTAM <89217282+nandinigautamgithub@users.noreply.github.com>
2021-10-08 14:33:14 +0530

    java

---
+++ TwoDArray.java
@@ -0,0 +1,16 @@
+package com.CoreJava;
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		
+		int[][] Array = { {12, 22},{45, 65}};
+
+		System.out.println("Element at Array[0][0] = " + Array[0][0]);
+		System.out.println("Element at Array[0][1] = " + Array[0][1]);
+		System.out.println("Element at Array[1][0] = " + Array[1][0]);
+		System.out.println("Element at Array[1][1] = " + Array[1][1]);
+
+	}
+
+}
---
+++ distance.java
@@ -0,0 +1,31 @@
+package com.CoreJava;
+
+import java.util.Scanner;
+
+public class distance {
+	public static void main(String [] args) {
+	
+		 int x,y;
+		 
+         double dis;
+
+         Scanner sc=new Scanner(System.in);
+
+         System.out.println("enter x point");
+   
+             x=sc.nextInt();
+    
+             System.out.println("enter y point");
+   
+             y=sc.nextInt();
+
+         
+  	    
+	 dis=Math.sqrt(x*x + y*y);
+	 
+	 	    	 	    
+	         System.out.println("\" + x + \", \" + y + \") to (0, 0) = \" + dist===>"+dis);
+
+}
+}
+
